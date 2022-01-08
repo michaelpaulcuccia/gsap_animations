471216f5969f21c7873a13100c897c570ea332d5
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2022-01-08 18:07:39 -0500

    updated about

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import styles from '../styles/About.module.css';
-import Slider from '../components/Slider';
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/About.module.css";
+import Slider from "../components/Slider";
 
 export default function About() {
+  const router = useRouter();
+  const path = router.pathname;
+  const cleanPath = path.substring(1);
 
-    const router = useRouter();
-    const path = router.pathname;
-    const cleanPath = path.substring(1);
-
-    return (
-        <div className={styles.wrapper}>
-            <Slider path={cleanPath} />
-                <div className={styles.content}>
-                <h2>Hello! My name is Michael-Paul Cuccia</h2>
-                <p>I am a Frontend Engineer at <Link href='https://www.designory.com/'>designory.</Link></p>
-                <p>this <strong>desktop</strong> application is built with Next.js</p>
-                <p>animations by GreenSock (gsap)</p>
-                <p>'wusignory' logo and hamburger button made by me</p>
-                <p>using Figma</p>
-                <p>otherwise, open source</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.wrapper}>
+      <Slider path={cleanPath} />
+      <div className={styles.content}>
+        <h2>Hello! My name is Michael-Paul Cuccia</h2>
+        <p>
+          I am a Frontend Engineer at{" "}
+          <Link href="https://www.designory.com/">designory.</Link>
+        </p>
+        <p>
+          this <strong>desktop</strong> application is built with Next.js
+        </p>
+        <p>animations by GreenSock (gsap)</p>
+        <p>cookie, logo, and hamburger button were made</p>
+        <p>using Figma</p>
+        <p>otherwise, open source</p>
+      </div>
+    </div>
+  );
 }
