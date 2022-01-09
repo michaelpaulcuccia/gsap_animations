060c0d876615ccf06bdb32e12e7362cbd14f8d28
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2022-01-09 14:44:58 -0500

    registration connected and working!

--- client/context/AuthContext.js
+++ client/context/AuthContext.js
@@ -1,43 +1,52 @@
-import { createContext, useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 //create context
 const AuthContext = createContext();
 
 //provider to wrap application
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
 
-    const [user, setUser] = useState(null);
-    const router = useRouter();
+  //register user
+  const register = async ({ username, email, password, sendPromotions }) => {
+    //create new user object
+    const user = { username, email, password, sendPromotions };
+    const res = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!res.ok) {
+      throw new Error(res.status);
+    } else {
+      console.log("ok");
+    }
+  };
 
-    //register user
-    const register = async ({username, email, password, sendPromotions}) => {
-        console.log(`AUTH CONTEXT: ${username}`);
-        console.log(`AUTH CONTEXT: ${email}`);
-        console.log(`AUTH CONTEXT: ${password}`);
-        console.log(`AUTH CONTEXT: ${sendPromotions}`);
-    };
+  //login user
+  const login = async ({ username, password }) => {
+    console.log(username, password);
+  };
 
-    //login user
-    const login = async ({ username, password }) => {
-        console.log(username, password);
-    };
+  //logout user
+  const logout = async () => {
+    console.log("logout");
+  };
 
-    //logout user
-    const logout = async () => {
-        console.log('logout');
-    };
+  //check if user is logged in to persist
+  const checkUserLoggedIn = async () => {
+    console.log("check");
+  };
 
-    //check if user is logged in to persist
-    const checkUserLoggedIn = async () => {
-        console.log('check');
-    };
+  return (
+    <AuthContext.Provider value={{ user, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-    return(
-        <AuthContext.Provider value={{user, register, login, logout}}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
-export default AuthContext
+export default AuthContext;
