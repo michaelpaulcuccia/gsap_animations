0636e06789e8fe5a703edb02dd1fb833bf086319
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-10 17:16:48 -0500

    didnt work, try again from docs

--- client/pages/login.js
+++ client/pages/login.js
@@ -19,9 +19,7 @@ export default function Login({allusers}) {
 
 export async function getStaticProps() {
 
-  //process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
-
-  const res = await fetch(process.env.NEXT_PUBLIC_FRONTEND_URL ||'http://localhost:3000/api/allusers');
+  const res = await fetch('http://localhost:3000/api/allusers');
   const allusers = await res.json();
 
   return {
