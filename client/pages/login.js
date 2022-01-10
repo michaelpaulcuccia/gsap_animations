16282ed106cdd1d84c3e15389949e415af02f5ba
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-10 16:55:06 -0500

    updated api route

--- client/pages/login.js
+++ client/pages/login.js
@@ -18,7 +18,7 @@ export default function Login({allusers}) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/allusers');
+  const res = await fetch('http://localhost:3000/api/allusers' || 'http://nextdesignsystem.vercel.app/api/allusers');
   const allusers = await res.json();
 
   return {
