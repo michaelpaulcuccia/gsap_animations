df22933d08e7af5c417da74e60e4e6de7338519e
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-10 17:06:05 -0500

    updated alluser api

--- client/pages/api/allusers.js
+++ client/pages/api/allusers.js
@@ -5,7 +5,8 @@ export default async (req, res) => {
   await dbConnect();
     
   const data = await User.find({});
-  res.status(201).json(data);
+  //res.status(201).json(data);
+  res.status(200).json(JSON.stringify(data))
  
 }
 
